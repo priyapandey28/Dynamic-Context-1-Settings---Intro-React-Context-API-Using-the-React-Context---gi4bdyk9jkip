0933e3ac5fda336fcfa637d250449e9d69ebbe11
priyapandey28 <121967472+priyapandey28@users.noreply.github.com>
2023-01-04 23:52:02 +0530

    Add files via upload

---
+++ src/context/userContext.js
@@ -0,0 +1,17 @@
+import React, { createContext, useState } from "react";
+
+
+const UserContext = createContext()
+
+const Wrapper = (props) => {
+
+    // const greeting = "Hello"
+    const [greeting,changeGreeting] = useState("Hello")
+  
+    return (
+        <UserContext.Provider value={{ greeting,changeGreeting }}>
+            {props.children}
+        </UserContext.Provider>
+    )
+}
+export { Wrapper, UserContext }
